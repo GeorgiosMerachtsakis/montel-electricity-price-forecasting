11b254e7bbbf2142957ccfd55e1b307b74bab776
GeorgiosMerachtsakis <merachtsakis.georgios@gmail.com>
2025-07-08 03:36:43 +0200

    Add files via upload

---
+++ Neural network prediction.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 15:21:19 2022
+
+@author: nicop
+"""
+
+#%%
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import datetime as dt
+
+from darts.timeseries import TimeSeries as DTS
+
+
+from darts.models import TCNModel
+from darts.utils.likelihood_models import LaplaceLikelihood
+
+from darts.dataprocessing.transformers import Scaler
+from sklearn.preprocessing import MinMaxScaler
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+
+from darts.metrics import mape
+from darts.metrics import rmse
+
+#%%
+
+def filter_time(df, t_start, t_end):
+    """
+    Function to filter a dataframe between t_start and t_end where time is the index
+    """
+    df = df.loc[ (df.index.values>=t_start) & (df.index.values<=t_end)]
+    
+    return df
+
+def processdata_prices(df, t_start, t_end):
+    """
+    Function to process prices
+    """
+    
+    # Convert HourUTC to timestamp
+    df['HourUTC'] = pd.to_datetime(df['HourUTC'])
+    # Remove timezone
+    df['HourUTC'] = df['HourUTC'].dt.tz_localize(None)
+    # Set HourUTC as index
+    df.set_index('HourUTC', inplace = True)
+    # Filter the dataframe for time
+    df = filter_time(df, t_start, t_end)
+    # Keep only the price areas needed
+    df = df.loc[(df['PriceArea']=="DK2")]
+    # Sort the dataframe
+    df = df.sort_index()
+    # Remove useless columns
+    df = df[['SpotPriceDKK']]
+    return df
+
+#%% Set start and end date for data processing
+t_start = pd.to_datetime(dt.datetime(2019, 1, 1, 0, 0, 0))
+t_end   = pd.to_datetime(dt.datetime(2021, 12, 31, 23, 0, 0))
+
+os.chdir("C:/Users/nicop/OneDrive/Documents/DTU/3 - 2022 Spring/31762 Introduction to Energy Analytics/Assignments/Assignment 2")
+
+cwd = os.getcwd()
+price_dir = os.path.join(cwd,'elspotprices_19to21.csv')
+df_price = pd.read_csv(price_dir)
+
+df_price = processdata_prices(df_price, t_start, t_end)
+
+exo_dir = os.path.join(cwd,'exogenousvariables_19to21.csv')
+df_exo = pd.read_csv(exo_dir)
+
+#%%
+
+# Convert HourUTC to timestamp
+df_exo['HourUTC'] = pd.to_datetime(df_exo['HourUTC'])
+# Remove timezone
+df_exo['HourUTC'] = df_exo['HourUTC'].dt.tz_localize(None)
+# Set HourUTC as index
+df_exo.set_index('HourUTC', inplace = True)
+# Filter the dataframe for time
+df_exo = filter_time(df_exo, t_start, t_end)
+# Keep only the price areas needed
+df_exo = df_exo.loc[(df_exo['PriceArea']=="DK2")]
+# Create combined variables
+columns = df_exo.columns
+df_exo['WindPower'] = np.sum(df_exo[col] for col in columns[2:6])
+df_exo['HydroPower'] = df_exo[columns[6]]
+df_exo['SolarPower'] = np.sum(df_exo[col] for col in columns[7:])
+# Sort the dataframe
+df_exo = df_exo.sort_index()
+# Remove useless columns
+df_exo = df_exo.drop(columns=[col for col in columns])
+
+#%% 
+series_price = DTS.from_dataframe(df_price)
+series_exo   = DTS.from_dataframe(df_exo)
+
+#%%
+#transforming data
+scaler = Scaler()
+
+# Transforming the time series objective
+series_price_transformed = scaler.fit_transform(series_price)
+
+# Transforming the time series exogenous
+series_exo_transformed   = scaler.fit_transform(series_exo)
+
+#%%
+# TRAIN - TEST SPLIT
+train_test_split = "20211130T23" # data will be split after  
+train_val_price_transformed, test_price_transformed = series_price_transformed.split_after(pd.Timestamp(train_test_split))
+
+
+# TRAIN - VAL SPLIT
+train_val_split = "20211115T23" # data will be split after  
+train_price_transformed, val_price_transformed = train_val_price_transformed.split_after(pd.Timestamp(train_val_split))
+
+#%%
+plt.figure(figsize=(35,10))
+train_price_transformed.plot()
+val_price_transformed.plot()
+test_price_transformed.plot()
+
+#%%
+day_series = datetime_attribute_timeseries(series_price_transformed, attribute = 'hour', one_hot = True)
+scaler_day = Scaler()
+day_series_transformed = scaler_day.fit_transform(day_series)
+
+series_exo_all_transformed = day_series_transformed.stack(series_exo_transformed)
+
+#%% Find best input length value
+
+for days in range(7):
+    model_CNN = TCNModel(
+        input_chunk_length=days*24,
+        output_chunk_length=24,
+        n_epochs = 14,
+        dropout = 0.1,
+        dilation_base = 2,
+        weight_norm = True,
+        kernel_size = 3,
+        num_filters = 8,
+        nr_epochs_val_period = 1, 
+        random_state = 0,
+    )
+    
+    # Get training and validation loss for each epoch
+
+#%% Model definition
+#For more info: https://unit8co.github.io/darts/generated_api/darts.models.forecasting.tcn_model.html
+model_CNN = TCNModel(
+    input_chunk_length=7*24,
+    output_chunk_length=24,
+    n_epochs = 14,
+    dropout = 0.1,
+    dilation_base = 2,
+    weight_norm = True,
+    kernel_size = 3,
+    num_filters = 8,
+    nr_epochs_val_period = 1, 
+    random_state = 0,
+)
+
+#%% Model fitting
+#model_CNN.fit(series=train_price_transformed, past_covariates=allCov, verbose=True)
+model_CNN.fit(series=train_price_transformed,
+             past_covariates = series_exo_all_transformed,
+             val_series = val_price_transformed,
+             val_past_covariates = series_exo_all_transformed,
+             verbose=True)
+
+
+#%% Prediction all at a time
+train_test_split = pd.Timestamp('20211130T23')
+train_price_transformed, test_price_transformed = series_price_transformed.split_after(train_test_split)
+
+# Second fit
+model_CNN.fit(series = train_price_transformed, past_covariates = series_exo_all_transformed, verbose = True)
+
+# past covariants should also cover the last 'input_chunk_length' of data before the test date begins.
+window = 24
+prediction = model_CNN.predict(window, past_covariates = series_exo_all_transformed ).pd_dataframe()
+
+test_start = pd.Timestamp('20211201')
+test_end = test_start + pd.Timedelta(hours = window)
+target = series_price_transformed[test_start:test_end].pd_dataframe()
+print("RMSE = {:.2f}%".format(rmse(DTS.from_dataframe(target), DTS.from_dataframe(prediction))))
+
+plt.figure(figsize=(8,4))
+plt.plot(target, label = "Real price [DKK]")
+plt.plot(prediction.SpotPriceDKK, label="Predicted price [DKK] (H=24)")
+plt.legend()
+
+#%% Prediction day by day
+train_end = pd.Timestamp('20211130T23')
+test_start = pd.Timestamp('20211201')
+test_end = pd.Timestamp('20211231T23')
+prediction = pd.DataFrame()
+
+for day in range(31):
+    train_test_split = train_end + pd.Timedelta(days = day)
+    train_price_transformed, test_price_transformed = series_price_transformed.split_after(train_test_split)
+    
+    model_CNN.fit(series = train_price_transformed, past_covariates = series_exo_all_transformed)
+    
+    day_prediction = model_CNN.predict(24, past_covariates = series_exo_all_transformed ).pd_dataframe()
+    prediction = pd.concat([prediction, day_prediction])
+
+    d = day+1
+    print("Predicted day %s" %d)
+
+target = series_price_transformed[test_start:test_end].pd_dataframe()
+
+plt.figure(figsize=(8,4))
+plt.plot(target, label = "Real price", color = 'black')
+plt.plot(prediction, label = "Predicted price", color = 'blue')
+plt.legend()
+
+print("RMSE = {:.2f}%".format(rmse(DTS.from_dataframe(target), DTS.from_dataframe(prediction))))
---
+++ SARIMAX prediction.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  3 15:37:38 2022
+
+@author: malthethingvad
+"""
+#%% 1.0 Process data
+import pandas as pd
+import pmdarima as pm
+from pmdarima import model_selection
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from pmdarima.utils import plot_acf
+from pmdarima.utils import plot_pacf
+import numpy as np
+
+# DATA IMPORT
+df = pd.read_csv('elspotprices_19to21.csv')
+df_X = pd.read_csv('exogenousvariables_19to21.csv')
+# Convert HourUTC to timestamp
+df['HourUTC'] = pd.to_datetime(df['HourUTC'])
+df_X['HourUTC'] = pd.to_datetime(df_X['HourUTC'])
+# Remove timezone
+df['HourUTC'] = df['HourUTC'].dt.tz_localize(None)
+df_X['HourUTC'] = df_X['HourUTC'].dt.tz_localize(None)
+# Set HourUTC as index
+df.set_index('HourUTC', inplace = True)
+df_X.set_index('HourUTC', inplace = True)
+# Keep only DK2
+df = df.loc[df["PriceArea"]=="DK2"]
+df_X = df_X.loc[df_X["PriceArea"]=="DK2"]
+# Sort the dataframe
+df = df.sort_index()
+df_X = df_X.sort_index()
+# Choose timeframe
+df = df.loc[ (df['HourDK']>'2019-01-01T01:00:00') & (df['HourDK']<='2020-01-01T01:00:00')]
+df_X = df_X.loc[ (df_X['HourDK']>'2019-01-01T01:00:00') & (df_X['HourDK']<='2020-01-01T01:00:00')]
+
+# Split data into train and test pieces
+df_spot = df[['SpotPriceEUR']].to_numpy()
+df_wind = df_X[['OffshoreWindGe100MW_MWh']].to_numpy()
+train, test = model_selection.train_test_split(df_spot, train_size=8184)
+train_X, test_X = model_selection.train_test_split(df_wind, train_size=8184)
+
+# Plot data 
+plt.figure(0)
+plt.plot(train)
+plt.title('Training set data')
+plt.xlabel('time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price for DK2', 'Data length', 'Total message length'),
+           loc='upper center', shadow=True)
+plt.show()
+
+# Plot ACF
+plt.figure(1)
+plot_acf(train, lags=48, title = "ACF")
+plt.show()
+# Plot PACF
+plt.figure(2)
+plot_pacf(train, lags=48, title = "PACF")
+plt.show()
+
+
+#%%####################################   1.1 - ARIMA model   ###############################################
+# Model SARIMAX(5, 1, 5)
+#arima = pm.auto_arima(train, error_action='ignore', trace=True, 
+#                      suppress_warnings=True, maxiter=20,
+#                      seasonal=False)
+arima = pm.auto_arima(train, start_p= 5, start_q = 5, d = 1,
+                      seasonal=False, trace = True)
+
+arima.plot_diagnostics(lags = 24)
+print(arima.summary())
+
+#%% Hour Ahead prediction ARIMA
+preds_all_arima_hour = []
+for t in test:
+    # Make prediction
+    preds, conf_int = arima.predict(n_periods=1, return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    preds_all_arima_hour.append(preds)
+    # Update the model with new measurements
+    arima.update(t)
+
+# Calculate RMS        
+rmse_arima_hour = sqrt(mean_squared_error(test, preds_all_arima_hour))
+print('RMSE ARIMA Hourly: %.3f' % rmse_arima_hour)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test)), test)  
+plt.plot(range(0,len(test)), preds_all_arima_hour)  
+plt.title('Hourly prediction with ARIMA model')
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+#%% Day ahead prediction ARIMA
+preds_all_arima_day = np.empty(0)
+k = 0
+for t in range(24-1):
+    # Make prediction
+    preds, conf_int = arima.predict(n_periods=24, return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    #preds_all_arima_day.append(,preds)cl
+    preds_all_arima_day = np.append(preds_all_arima_day, preds)
+    # Update the model with new measurements
+    arima.update(test[k:k+24])
+    k = k+24
+# Calculate RMS      
+rmse_arima_day = sqrt(mean_squared_error(test, preds_all_arima_day))
+print('Test RMSE: %.3f' % rmse_arima_day)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test)), test)  
+plt.plot(range(0,len(test)), preds_all_arima_day)
+plt.title('Daily prediction with ARIMA model')  
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+
+
+#%%####################################   1.2 - SARIMA model   ###############################################
+# Best model SARIMA(1,1,0)(2,0,0)[24]
+#arimaS = pm.auto_arima(train, error_action='ignore', trace=True, 
+#                      suppress_warnings=True, maxiter=20,
+#                      seasonal=True, m = 24)
+arimaS = pm.auto_arima(train, start_p=1, start_P=2, start_q = 0, start_Q=0, d = 1, D=0,
+                      seasonal=True, m = 24, trace = True)
+
+arimaS.plot_diagnostics(lags = 24)
+print(arimaS.summary())
+#%% Hour Ahead prediction SARIMA
+preds_all_arimaS_hour = []
+for t in test:
+    # Make prediction
+    preds, conf_int = arimaS.predict(n_periods=1, return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    preds_all_arimaS_hour.append(preds)
+    # Update the model with new measurements
+    arimaS.update(t)
+
+# Calculate RMS        
+rmse_arimaS_hour = sqrt(mean_squared_error(test, preds_all_arimaS_hour))
+print('RMSE SARIMA Hourly: %.3f' % rmse_arimaS_hour)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test)), test)  
+plt.plot(range(0,len(test)), preds_all_arimaS_hour)  
+plt.title('Hourly prediction with SARIMA model')
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+#%% Day ahead prediction SARIMA
+preds_all_arimaS_day = np.empty(0)
+k = 0
+for t in range(24-1):
+    # Make prediction
+    preds, conf_int = arimaS.predict(n_periods=24, return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    #preds_all_arima_day.append(,preds)cl
+    preds_all_arimaS_day = np.append(preds_all_arimaS_day, preds)
+    # Update the model with new measurements
+    arimaS.update(test[k:k+24])
+    k = k+24
+# Calculate RMS      
+rmse_arimaS_day = sqrt(mean_squared_error(test, preds_all_arimaS_day))
+print('Test RMSE: %.3f' % rmse_arimaS_day)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test)), test)  
+plt.plot(range(0,len(test)), preds_all_arimaS_day)
+plt.title('Daily prediction with SARIMA model')  
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+
+#%%####################################   1.3 - SARIMAX model   ###############################################
+arimaSX = pm.auto_arima(train, error_action='ignore', trace=True, 
+                      suppress_warnings=True, maxiter=20,
+                      seasonal=True, m = 24, exogenous = train_X)
+
+arimaSX.plot_diagnostics(lags = 24)
+
+print(arimaSX.summary())
+#%% Hour Ahead prediction SARIMA
+preds_all_arimaSX_hour = []
+k = 0
+for t in test:
+    # Make prediction
+    preds, conf_int = arimaSX.predict(n_periods=1, X = test_X[k].reshape(-1,1), return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    preds_all_arimaSX_hour.append(preds)
+    # Update the model with new measurements
+    arimaSX.update(t,test_X[k].reshape(-1,1))
+    print(k)
+    k = k+1
+# Calculate RMS        
+rmse_arimaSX_hour = sqrt(mean_squared_error(test, preds_all_arimaSX_hour))
+print('RMSE SARIMA Hourly: %.3f' % rmse_arimaSX_hour)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test)), test)  
+plt.plot(range(0,len(test)), preds_all_arimaSX_hour)  
+plt.title('Hourly prediction with SARIMAX model')
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+#%% Day ahead prediction SARIMA
+preds_all_arimaSX_day = np.empty(0)
+k = 0
+for t in range(23):
+    # Make prediction
+    preds, conf_int = arimaSX.predict(n_periods=24, X = test_X[k:k+24].reshape(-1,1), return_conf_int=True, alpha = 0.05)
+    # Append to forecast lists
+    #preds_all_arima_day.append(,preds)cl
+    preds_all_arimaSX_day = np.append(preds_all_arimaSX_day, preds)
+    # Update the model with new measurements
+    arimaSX.update(test[k:k+24],test_X[k:k+24].reshape(-1,1))
+    k = k+24
+#%% Calculate RMS      
+rmse_arimaSX_day = sqrt(mean_squared_error(test[0:552], preds_all_arimaSX_day))
+print('Test RMSE: %.3f' % rmse_arimaSX_day)
+# Plot prediction and test data
+plt.figure(3)
+plt.plot(range(0,len(test[0:552])), test[0:552])  
+plt.plot(range(0,len(test[0:552])), preds_all_arimaSX_day)
+plt.title('Daily prediction with SARIMAX model')  
+plt.xlabel('Time (Hours)')
+plt.ylabel('Price (Euro)')
+plt.legend(('Spot price real', 'Spot price prediction', 'Total message length'),
+           loc='best')
+plt.show()
+
+#%% Save predictions
+from numpy import savetxt
+savetxt('SARIMAX_prediction_daily.csv', preds_all_arimaSX_day, delimiter=',')
+savetxt('SARIMAX_prediction_hourly.csv', preds_all_arimaSX_hour, delimiter=',')
